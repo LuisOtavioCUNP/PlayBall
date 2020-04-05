e6df6240cea32adfc7f39641fd47c31c13ee4769
Luís <Lus@DESKTOP-H0T26Q8>
2020-04-04 21:08:33 -0300

    feat: Cria objetos e metodos

--- src/playball/modelo/Quadra.java
+++ src/playball/modelo/Quadra.java
@@ -10,5 +10,65 @@ package playball.modelo;
  * @author Luís
  */
 public class Quadra {
-    
+ private String nome;
+ private String area;
+ private String valorHora;
+ private String endereco;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the area
+     */
+    public String getArea() {
+        return area;
+    }
+
+    /**
+     * @param area the area to set
+     */
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    /**
+     * @return the valorHora
+     */
+    public String getValorHora() {
+        return valorHora;
+    }
+
+    /**
+     * @param valorHora the valorHora to set
+     */
+    public void setValorHora(String valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+ 
 }
