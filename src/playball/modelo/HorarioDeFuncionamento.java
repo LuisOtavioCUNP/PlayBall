ece2233672682eeb14e3be693b18c336346e9f26
Victor <Victor@DESKTOP-KPHM8PE>
2020-04-08 20:03:30 -0300

    teste

--- src/playball/modelo/HorarioDeFuncionamento.java
+++ src/playball/modelo/HorarioDeFuncionamento.java
@@ -1,15 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*SUB-CLASSE DE AGENDAMENTO*/
 
 package playball.modelo;
+
+import java.util.Calendar;
 
 /**
  * 
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 public class HorarioDeFuncionamento {
+
+    /**
+     * @return the inicioFuncionamento
+     */
+    public Calendar getInicioFuncionamento() {
+        return inicioFuncionamento;
+    }
+
+    /**
+     * @param inicioFuncionamento the inicioFuncionamento to set
+     */
+    public void setInicioFuncionamento(Calendar inicioFuncionamento) {
+        this.inicioFuncionamento = inicioFuncionamento;
+    }
+
+    /**
+     * @return the fimFuncionamento
+     */
+    public Calendar getFimFuncionamento() {
+        return fimFuncionamento;
+    }
+
+    /**
+     * @param fimFuncionamento the fimFuncionamento to set
+     */
+    public void setFimFuncionamento(Calendar fimFuncionamento) {
+        this.fimFuncionamento = fimFuncionamento;
+    }
+    private Calendar inicioFuncionamento;
+    private Calendar fimFuncionamento;
+    
+    
 
 }
