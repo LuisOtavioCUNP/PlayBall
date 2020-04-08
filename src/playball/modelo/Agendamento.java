20220b334a1165f87bb893a3da2db8a28b77e2d5
Raphael <Raphael@Junior-PC>
2020-04-08 16:30:32 -0300

    Criar classe agendamento

---
+++ src/playball/modelo/Agendamento.java
@@ -0,0 +1,74 @@
+
+package playball.modelo;
+
+
+public class Agendamento {
+    
+    private String vagas;
+    private String horaInicio;
+    private String horaFim;
+    private String diaAgendamento;
+
+    /**
+     * @return the vagas
+     */
+    public String getVagas() {
+        return vagas;
+    }
+
+    /**
+     * @param vagas the vagas to set
+     */
+    public void setVagas(String vagas) {
+        this.vagas = vagas;
+    }
+
+    /**
+     * @return the horaInicio
+     */
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    /**
+     * @param horaInicio the horaInicio to set
+     */
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    /**
+     * @return the horaFim
+     */
+    public String getHoraFim() {
+        return horaFim;
+    }
+
+    /**
+     * @param horaFim the horaFim to set
+     */
+    public void setHoraFim(String horaFim) {
+        this.horaFim = horaFim;
+    }
+
+    /**
+     * @return the diaAgendamento
+     */
+    public String getDiaAgendamento() {
+        return diaAgendamento;
+    }
+
+    /**
+     * @param diaAgendamento the diaAgendamento to set
+     */
+    public void setDiaAgendamento(String diaAgendamento) {
+        this.diaAgendamento = diaAgendamento;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
